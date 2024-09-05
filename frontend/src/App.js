3a84cd6697b26d8d21181050434d764450212607
williamapc <w.h.b.a.p.c@gmail.com>
2024-09-06 00:45:32 +0200

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Images Gallery</h1>
-//     </div>
-//   );
-// }
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
-// export default App;
+import Search from './components/Search';
+import { useState } from 'react';
 
 
-const App = () =>{
+const App = () => {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+  
   return (
-    <div className="App">
+    <div>
       <Header title="Gallery of Image From Unsplash"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Container } from 'react-bootstrap'
+
+
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+}
 
 
 const Header = (props) =>{
    return (
-        <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/"> {props.title}</Navbar.Brand>  
+        <Navbar style={navbarStyle} data-bs-theme="light">
+          <Container>
+            <Navbar.Brand href="/"> {props.title}</Navbar.Brand>  
+          </Container>
         </Navbar>
    )
       };
