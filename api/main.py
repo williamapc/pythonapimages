5ef4bdfbfe894939af107e53d12b785e0f3aefce
williamapc <w.h.b.a.p.c@gmail.com>
2024-09-15 15:52:35 +0200

    enable debug mode in flask app

--- api/main.py
+++ api/main.py
@@ -7,11 +7,14 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+# For debug mode set DEBUG at true in your local env file
+DEBUG = bool(os.environ.get("DEBUG", False)) 
 
 if not UNSPLASH_KEY:
-   raise EnvironmentError("Please create a local env file with your Secret key of Unsplash API")
+   raise EnvironmentError("Please create a local env file with your Unsplash API's Secret key")
 
 app = Flask(__name__)
+app.config["DEBUG"] = DEBUG
 
 
 @app.route("/new-image")
@@ -31,4 +34,4 @@ def new_image():
   return data
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 5050, debug=True)
+    app.run("0.0.0.0", 5050)
