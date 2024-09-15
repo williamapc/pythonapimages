928eba9a3b30b8541b2db2a4e0105a9f764bc3d1
williamapc <w.h.b.a.p.c@gmail.com>
2024-09-15 15:16:04 +0200

    added request to Unsplash API

--- api/main.py
+++ api/main.py
@@ -1,11 +1,34 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+   raise EnvironmentError("Please create a local env file with your Secret key of Unsplash API")
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+
+  headers = {
+     "Accept-Version": "v1",
+     "Authorization": "Client-ID " + UNSPLASH_KEY
+  }
+ 
+  params = {
+     "query": word
+  }
+  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+  data = response.json()
+  return data
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 5050)
+    app.run("0.0.0.0", 5050, debug=True)
